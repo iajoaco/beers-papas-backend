9352a4c952c534e24b07c127453a451e3c76fab9
joacoia <joaquin.gsanmiguel@cunef.edu>
2025-05-03 18:39:27 +0200

    .

---
+++ src/main/java/com/example/beerspapasbackend/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.beerspapasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.beerspapasbackend.repository")
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+} 
