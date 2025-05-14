750d6b7e965a54a275d3cc1c540bd6d0b675dad3
joacoia <joaquin.gsanmiguel@cunef.edu>
2025-05-14 20:31:33 +0200

    registro_celdas

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -128,8 +128,13 @@ function addProductMarker(product) {
         map: map,
         title: product.name,
         icon: {
-            url: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
-            scaledSize: new google.maps.Size(32, 32)
+            path: "M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z",
+            fillColor: "#FF5252",
+            fillOpacity: 1,
+            strokeColor: "#ffffff",
+            strokeWeight: 2,
+            scale: 1.5,
+            anchor: new google.maps.Point(12, 24)
         }
     });
 
